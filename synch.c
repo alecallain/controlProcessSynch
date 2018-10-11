01e351c478033ff91fa9009158c549094f8fab33
Alec Allain <allainal@eos22.cis.gvsu.edu>
2018-10-11 00:54:03 -0400

    My latest changes

--- synch.c
+++ synch.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/ipc.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <sys/types.h> 
+#include <sys/wait.h> 
+#include <sys/ipc.h> 
 #include <sys/shm.h>
 #include <sys/stat.h>
-#include <pthread.h>
-#include <semaphore.h>
+#include <string.h>
 #include <sys/sem.h>
 
 #define SIZE 16
@@ -16,77 +15,106 @@
 * This class is for understanding the use
 * of proper synchronization mechanisms
 *
-* @author Allsion Bolen
+* @author Allison Bolen
 * @author Alec Allain
 * @version 10/7/18
 */ 
 
-/** Global variables */
-int shmId;
-long int* shmPtr;
-pid_t pid;
-sem_t mutex;
-int status;
-int semId;
-int loop
+/** Global varaibles */
 struct sembuf semBuffer;
 
 /** Instanciating methods */
-void sigHandler(int input);
 
 /**
 * Main method
-*/
-int main (int argc, char** argv) {
-	signal(SIGINT, sigHandler);
+*/ 
+int main (int argc, char** argv) { 
+   int status; 
+   long int i, loop, temp, *shmPtr; 
+   int shmId; 
+   pid_t pid;
+   int semId;
 
-	if ((shmId = shmget (IPC_PRIVATE, SIZE, IPC_CREAT|S_IRUSR|S_IWUSR)) < 0 ) {
-		fprintf(stderr, "Error getting ID\n");
-		exit(1);
-	} else if ((shmPtr = shmat (shmId, 0, 0)) == (void*) -1) {
-		fprintf(stderr, "\n");
-		exit(1);
-	}
+   // Creates new semaphore
+   semId = semget (IPC_PRIVATE, 1, 00600);
 
-	printf("\nCreating a semaphore\n");
+   // Initializes the semaphore set
+   semctl (semId, 0, SETVAL, 1);
 
-	// creates a new semaphore 
-	semId = semget (IPC_PRIVATE, 1, 600);
+      // get value of loop variable (from command-line argument)
+   loop = atoi(argv[1]);
 
-	// initializes the semaphore set
-	semctl (semId, 0, SETVAL, 1);
+   if ((shmId = shmget (IPC_PRIVATE, SIZE, IPC_CREAT|S_IRUSR|S_IWUSR)) < 0) {
+      perror ("i can't get no..\n"); 
+      exit (1); 
+   } 
+   if ((shmPtr = shmat (shmId, 0, 0)) == (void*) -1) { 
+      perror ("can't attach\n"); 
+      exit (1); 
+   }
 
-	// performs operations on elements
-	semop(semId, &semBuffer, 1);
+   shmPtr[0] = 0; 
+   shmPtr[1] = 1;
 
-	// removes the semaphore
-	semctl (semId, 0, IPC_RMID);
+   if (!(pid = fork())) { 
+      for (i=0; i<loop; i++) { 
+	      // swap the contents of shmPtr[0] and shmPtr[1]
+	      
+	      // Semaphore creates block signal
+	      semop (semId, semBuffer, 1);
 
-	wait(&status);
+	      memcpy(&temp, &shmPtr[0], sizeof(shmPtr));
+	      memcpy(&shmPtr[0], &shmPtr[1], sizeof(shmPtr));
+	      memcpy(&shmPtr[1], &temp, sizeof(shmPtr));
 
-	if (shmdt(shmPtr) < 0) {
-		fprintf(stderr, "Can't let go...\n");
-		exit(1);
-	} else if (shmctl(shmId, IPC_RMID, 0) < 0) {
-		fprintf(stderr, "Memory can't be deallocated");
-		exit(1);
-	}
-}
+	      // Semaphore stops the block
+	      semop (semId, semBuffer, 1);
 
-/**
-* This method waits for the control-c 
-* function from the user
-*/
-void sigHandler (int input) {
-	printf("Exiting program...\nDeleting and detaching connections\n");
+	      // wrong
+	      //temp = shmPtr[0];
+	      //shmPtr[0] = shmPtr[1];
+	      //shmPtr[1] = temp;
+      } 
+      if (shmdt (shmPtr) < 0) { 
+         perror ("just can't let go\n"); 
+         exit (1); 
+      } 
+      exit(0); 
+   } 
+   else 
+      for (i=0; i<loop; i++) { 
+               // swap the contents of shmPtr[1] and shmPtr[0]   
+	       
+	       // Semaphore creates block signal
+	       semop (semId, semBuffer, 1);
 
-	if (shmdt(shmPtr) < 0) {
-		fprintf(stderr, "Can't let go...\n");
-		exit(1);
-	} else if (shmctl(shmId, IPC_RMID, 0) < 0) {
-		fprintf(stderr, "Memory can't be deallocated");
-		exit(1);
-	}
+	       memcpy(&temp, &shmPtr[1], sizeof(shmPtr));
+	       memcpy(&shmPtr[1], &shmPtr[0], sizeof(shmPtr));
+	       memcpy(&shmPtr[0], &temp, sizeof(shmPtr));
 
-	exit(0);
+	       // Semaphore stops block signal
+	       semop (semId, semBuffer, 1);
+
+	       // wrong
+	       //temp = shmPtr[1];
+	       //shmPtr[1] = shmPtr[0];
+	       //shmPtr[0] = temp; 
+      }
+
+   wait (&status); 
+   printf ("values: %li\t%li\n", shmPtr[0], shmPtr[1]);
+
+   if (shmdt (shmPtr) < 0) { 
+      perror ("just can't let go\n"); 
+      exit (1); 
+   } 
+   if (shmctl (shmId, IPC_RMID, 0) < 0) { 
+      perror ("can't deallocate\n"); 
+      exit(1); 
+   }
+
+   // Removes the semaphore
+
+
+   return 0; 
 }
